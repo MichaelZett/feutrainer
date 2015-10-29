29578aaae4bce81700f67327dc563e9f183dd35b
MichaelZett <michael2.zoeller@gmail.com>
2015-10-29 16:51:04 +0100

    Finish 0.2.0 * Table Renderer für Sets (OK) * Popup fixe größere Width (OK) * FormFields für Sets (OK)

--- parent/ui/src/main/java/de/zettsystems/feutrainer/ui/courses/CourseEntryForm.java
+++ parent/ui/src/main/java/de/zettsystems/feutrainer/ui/courses/CourseEntryForm.java
@@ -1,20 +1,36 @@
 package de.zettsystems.feutrainer.ui.courses;
 
-import com.vaadin.ui.Component;
+import org.vaadin.viritin.fields.MultiSelectTable;
 
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Table;
+
+import de.zettsystems.feutrainer.domain.base.BaseRepository;
 import de.zettsystems.feutrainer.domain.courses.Course;
+import de.zettsystems.feutrainer.domain.organisation.Chair;
 import de.zettsystems.feutrainer.domain.organisation.ChairRepository;
 import de.zettsystems.feutrainer.ui.base.AbstractBaseForm;
 
 public class CourseEntryForm extends AbstractBaseForm<Course> {
+	MultiSelectTable<Chair> chairs;
 
 	public CourseEntryForm(Course entry, ChairRepository repo) {
 		super(entry, repo);
 	}
 
 	@Override
+	protected void initializeAdditionalComponents(Course entry, BaseRepository<?> repository) {
+		ChairRepository repo = (ChairRepository) repository;
+		this.chairs = new MultiSelectTable<Chair>("Chairs").withProperties("id")
+				.withColumnHeaderMode(Table.ColumnHeaderMode.HIDDEN);
+		this.chairs.setHeight("120px");
+		this.chairs.setWidth("200px");
+		this.chairs.setOptions(repo.findAll());
+	}
+
+	@Override
 	protected Component[] getAdditionalFields() {
-		return new Component[0];
+		return new Component[] { this.chairs };
 	}
 
 }
